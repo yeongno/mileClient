127a5ed874286c74d58e735c813b6d165ba27ea0
yeongno <dldudsh83@gmail.com>
2022-08-23 21:29:00 +0900

    navBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
+import NavBar from "./views/navBar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
---
+++ src/components/views/navBar/NavBar.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import LeftMenu from "./Sections/LeftMenu";
+import RightMenu from "./Sections/RightMenu";
+import { Drawer, Button } from "antd";
+import "./Sections/NavBar.css";
+import { MenuOutlined } from "@ant-design/icons";
+
+function NavBar() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <nav
+      className="menu"
+      style={{ position: "fixed", zIndex: 5, width: "100%" }}
+    >
+      <div className="menu__logo">
+        <a href="/">Home</a>
+      </div>
+      <div className="menu__container">
+        <div className="menu_left">
+          <LeftMenu mode="horizontal" />
+        </div>
+        <div className="menu_rigth">
+          <RightMenu mode="horizontal" />
+        </div>
+
+        <Button
+          className="menu__mobile-button"
+          type="primary"
+          onClick={showDrawer}
+        >
+          <MenuOutlined type="align-right" />
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          className="menu_drawer"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+        >
+          <LeftMenu mode="inline" />
+          <RightMenu mode="inline" />
+        </Drawer>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
