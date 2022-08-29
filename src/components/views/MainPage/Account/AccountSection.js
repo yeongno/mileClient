b073a480d85fbdb6043bfdce81b4cb92b9d2f8d9
yeongno <dldudsh83@gmail.com>
2022-08-30 00:25:31 +0900

    account_section하는 중

--- src/components/views/MainPage/Account/AccountSection.js
+++ src/components/views/MainPage/Account/AccountSection.js
@@ -1,8 +1,34 @@
+import { CheckOutlined } from "@ant-design/icons";
 import React from "react";
 import "../../../styles/MainPage/Account.scss";
 
 function AccountSection() {
-  return <div className="container_account">sdfds</div>;
+  const Name = window.localStorage.getItem("name");
+  return (
+    <div className="container_account">
+      <div className="top_account">
+        <div className="leftTop_account">
+          <img src="/assets/profile.png" alt="" />
+        </div>
+        <div className="rightTop_account">
+          <div className="nameLabel_account">
+            <CheckOutlined />
+            {Name}{" "}
+          </div>
+          <hr />
+          <div className="pointLabel_account">pointLabel_account</div>
+        </div>
+      </div>
+      <div className="down_account">
+        <div className="leftDown_account">익명 활성화</div>
+        <div className="rightDown_account">
+          <button>회원정보 변경</button>
+          {" / "}
+          <button>로그 아웃</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AccountSection;
