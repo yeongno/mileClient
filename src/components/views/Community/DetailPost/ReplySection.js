42f1d181f72300068fec155e0c6a7a6fda063d2d
yeongno <dldudsh83@gmail.com>
2022-10-11 22:02:36 +0900

    ReplySection style

--- src/components/views/Community/DetailPost/ReplySection.js
+++ src/components/views/Community/DetailPost/ReplySection.js
@@ -1,26 +1,39 @@
+import { BorderOutlined } from "@ant-design/icons";
 import React from "react";
+import "../../../styles/CommunityPage/DetailPost/ReplySection.scss";
 
 function ReplySection() {
-  return <div className="ReplySectionContainer_DetailPost">
-    <div className="topLabelReplySection_DetailPost">
+  return (
+    <div className="ReplySectionContainer_DetailPost">
+      <div className="topLabelReplySection_DetailPost">
+        <a>댓글</a>
+        <a>3</a>
+        <a>최신순</a>
+        <a>
+          <BorderOutlined />
+        </a>
+      </div>
+      <div className="partitionReplySection_DetailPost" />
+      <div className="replyListReplySection_DetailPost">
+        <div className="topReplySection_DetailPost">
+          <div className="profileReplySection_DetailPost">
+            <img src={"/assets/profile.png"} />
+            <a>답글쓰기</a>
+          </div>
+          <div className="detailReplySection_DetailPost">
+            <a>난 자갈치</a>
+            <a>복무중</a>
+            <br />
+            <a>댓글이다.</a>
+            <br />
 
-    </div>
-    <div className="partitionReplySection_DetailPost" />
-    <div className="replyListReplySection_DetailPost">
-      <div className="topReplySection_DetailPost">
-        <div className="profileReplySection_DetailPost">
-
-        </div>
-        <div className="detailReplySection_DetailPost">
-          <a>난 자갈치</a>
-          <a>복무중</a>
-          <a>댓글이다.</a>
-          <a>2015.12:23</a>
-          <div className="subPartitionReplySection_DetailPost" />
+            <a>2015.12:23</a>
+          </div>
         </div>
       </div>
+      <div className="subPartitionReplySection_DetailPost" />
     </div>
-  </div>;
+  );
 }
 
 export default ReplySection;
