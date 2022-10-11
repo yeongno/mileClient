10e85fdd0c9eae56f600796083fae6047db4efd9
lyn <dldudsh83@gmail.com>
2022-10-11 16:51:08 +0900

    replySection 클래스명 지정

--- src/components/views/Community/DetailPost/ReplySection.js
+++ src/components/views/Community/DetailPost/ReplySection.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function ReplySection() {
-  return <div>ReplySection</div>;
+  return <div className="ReplySectionContainer_DetailPost">
+    <div className="topLabelReplySection_DetailPost">
+
+    </div>
+    <div className="partitionReplySection_DetailPost" />
+    <div className="replyListReplySection_DetailPost">
+      <div className="topReplySection_DetailPost">
+        <div className="profileReplySection_DetailPost">
+
+        </div>
+        <div className="detailReplySection_DetailPost">
+          <a>난 자갈치</a>
+          <a>복무중</a>
+          <a>댓글이다.</a>
+          <a>2015.12:23</a>
+          <div className="subPartitionReplySection_DetailPost" />
+        </div>
+      </div>
+    </div>
+  </div>;
 }
 
 export default ReplySection;
