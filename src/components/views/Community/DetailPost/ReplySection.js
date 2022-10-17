53b15a07959c61ffa1fa3376bc1ac22ce289fe63
lyn <dldudsh83@gmail.com>
2022-10-17 15:01:29 +0900

    댓글 수 구현

--- src/components/views/Community/DetailPost/ReplySection.js
+++ src/components/views/Community/DetailPost/ReplySection.js
@@ -1,14 +1,17 @@
 import { BorderOutlined } from "@ant-design/icons";
 import React from "react";
+import { useSelector } from "react-redux";
 import "../../../styles/CommunityPage/DetailPost/ReplySection.scss";
 import OnReply from "./OnReply";
 
 function ReplySection() {
+  const Reply = useSelector((state) => state.reply.getReply.req);
+  console.log(Reply.length)
   return (
     <div className="ReplySectionContainer_DetailPost">
       <div className="topLabelReplySection_DetailPost">
         <a>댓글</a>
-        <a>3</a>
+        <a>{Reply.length}</a>
         <a>최신순</a>
         <a>
           <BorderOutlined />
